f89d1629314b52cbb6123095d07e80c7afb19c2d
Anushka <anushkavarshney2708@gmail.com>
2025-04-13 21:31:15 +0530

    integrated firebase and fixed verification bug

---
+++ src/Firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
+
+
+const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: "medisync-300e5.firebaseapp.com",
+  projectId: "medisync-300e5",
+  storageBucket: "medisync-300e5.firebasestorage.app",
+  messagingSenderId: "241681212441",
+  appId: "1:241681212441:web:050c066f0cbb26ad62f688",
+  measurementId: "G-B3QM3DCKRV"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const doctorsCollection = collection(db, "doctors")
+export const patientsCollection = collection(db, "patients")
